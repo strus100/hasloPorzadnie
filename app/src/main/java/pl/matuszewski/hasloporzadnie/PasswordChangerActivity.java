a06ed7c01ada5c62905a304a732d51e8558764d5
strus100 <danek130@gmail.com>
2020-11-19 19:14:22 +0100

    Password changer basic created

---
+++ app/src/main/java/pl/matuszewski/hasloporzadnie/PasswordChangerActivity.java
@@ -0,0 +1,14 @@
+package pl.matuszewski.hasloporzadnie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PasswordChangerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_changer);
+    }
+}
