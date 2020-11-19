ab10760fb75efded395cb4bf13cb785761079b67
strus100 <danek130@gmail.com>
2020-11-19 18:59:01 +0100

    saving note

--- app/src/main/java/pl/matuszewski/hasloporzadnie/NoteActivity.java
+++ app/src/main/java/pl/matuszewski/hasloporzadnie/NoteActivity.java
@@ -2,14 +2,23 @@ package pl.matuszewski.hasloporzadnie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -17,13 +26,17 @@ public class NoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
-
         saveNote();
     }
 
     public void saveNote() {
+
         EditText noteEditText   = (EditText)findViewById(R.id.editTextTextMultiLine);
         TextView textView = (TextView) findViewById(R.id.textView2);
+
+        String note = readFromFile(getApplicationContext());
+        noteEditText.setText(note);
+
         noteEditText.addTextChangedListener(new TextWatcher() {
 
             @Override
@@ -33,10 +46,8 @@ public class NoteActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                String note = "";
-
-                note = noteEditText.getText().toString();
-                textView.setText(note);
+                String note = noteEditText.getText().toString();
+                writeToFile(note, getApplicationContext());
             }
 
             @Override
@@ -45,4 +56,44 @@ public class NoteActivity extends AppCompatActivity {
         });
     }
 
+    private String readFromFile(Context context) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput("config.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString).append("\n");
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret.substring(0,ret.length() - 1);
+    }
+
+    private void writeToFile(String data,Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("config.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
 }
